ad6124e323f87df1252dd28f2a1c625ba775ea06
Alex <alexlgwilliam@gmail.com>
2025-03-17 03:02:46 +0000

    Began work on pie chart of national identity

--- countyMapSetup.js
+++ countyMapSetup.js
@@ -346,7 +346,7 @@ function createRegionCharts(){
                         accessibility: {
                             point: {
                                 valueDescriptionFormat: '{index}. Age {xDescription}, {value}%.'
-                            }
+                            },
                         },
                         xAxis: [{
                             categories: categories,
